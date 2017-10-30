0b1332cc5175bf7cabe667b12625910a8c52a463
userjack6880 <john@systemanomaly.com>
2017-10-29 20:31:13 -0500

    test

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Configuration file for OBQ Data Scraper
+ */
+
+/**
+ * MySQL Settings
+ */
+
+define('DB_NAME', 'obq_db');
+define('DB_USER', 'obq_user');
+define('DB_PASSWORD', 'OyOhFyM4rPCjZgLt');
+define('DB_HOST', 'localhost');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+?>
---
+++ process.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Process HTML
+ */
+# Preload Stuff
+include_once 'lib/hquery.php';
+	use duzun\hQuery;
+	hQuery::$cache_path = "cache";
+
+$site = $_GET["site"];
+
+# Site Specific Stuff
+# pbia
+if ($site == 'pbia' || $site == 'all') {
+	# get full list of instructors
+	$doc = hQuery::fromUrl('http://playbetterbilliards.com/instructors?state=&country=&active=1',
+	                       ['Accept' => 'txt/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
+
+#	$links = array();
+
+	$entries = $doc->find('tr');
+
+	if ($entries) {
+		foreach($entries as $pos -> $tr) {
+		echo $tr->attr{'onclick');
+	}
+	# pull up each instructor's page and pull the data
+	# output
+}
+
+# Functions
+
+?>
