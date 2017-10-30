a86c38cca4519d18878248be4a98a1923f7e4e5c
userjack6880 <john@systemanomaly.com>
2017-10-29 20:34:45 -0500

    test

--- process.php
+++ process.php
@@ -9,21 +9,23 @@ include_once 'lib/hquery.php';
 
 $site = $_GET["site"];
 
+echo '\o/';
+
 # Site Specific Stuff
 # pbia
 if ($site == 'pbia' || $site == 'all') {
 	# get full list of instructors
-	$doc = hQuery::fromUrl('http://playbetterbilliards.com/instructors?state=&country=&active=1',
+#	$doc = hQuery::fromUrl('http://playbetterbilliards.com/instructors?state=&country=&active=1',
 	                       ['Accept' => 'txt/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
 
 #	$links = array();
 
-	$entries = $doc->find('tr');
+#	$entries = $doc->find('tr');
 
-	if ($entries) {
-		foreach($entries as $pos -> $tr) {
-		echo $tr->attr{'onclick');
-	}
+#	if ($entries) {
+#		foreach($entries as $pos -> $tr) {
+#		echo $tr->attr{'onclick');
+#	}
 	# pull up each instructor's page and pull the data
 	# output
 }
