c167016f4ceb0c2ccd15828bf491c8f489256a3a
userjack6880 <john@systemanomaly.com>
2017-10-29 21:17:11 -0500

    update

--- process.php
+++ process.php
@@ -14,17 +14,18 @@ $site = $_GET["site"];
 if ($site == 'pbia' || $site == 'all') {
 	# get full list of instructors
 	$doc = hQuery::fromUrl('http://playbetterbilliards.com/instructors?state=&country=&active=1',['Accept' => 'txt/html,application/xhtml+xml;q=0.9,*/*;q=0.8']);
-	echo 'loading site';
+	echo "loading site\n";
 
 #	$links = array();
 
 	$entries = $doc->find('tr');
 
 	if ($entries) {
-	echo 'entries found';
-#		foreach($entries as $pos -> $tr) {
+	echo "entries found\n";
+		foreach($entries as $pos -> $tr) {
+			echo "printing entry\n";
 #			echo $tr->attr{'onclick');
-#		}
+		}
 	}
 	# pull up each instructor's page and pull the data
 	# output
